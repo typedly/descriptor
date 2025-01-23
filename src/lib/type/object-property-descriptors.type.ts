acbe8f0126ec1d7331ffc0a59cc6941106ade829
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-23 17:23:37 +0000

    refactor(ObjectPropertyDescriptors): change the `x` to `key`.

--- src/lib/type/object-property-descriptors.type.ts
+++ src/lib/type/object-property-descriptors.type.ts
@@ -1,8 +1,17 @@
+/*
+  TODO:  Check the following code.
+  export type ObjectPropertyDescriptors<Obj> = {
+    [P in keyof Obj]: TypedPropertyDescriptor<Obj[P]>;
+  } & {
+    [K in Exclude<string, keyof Obj>]?: PropertyDescriptor;
+  };
+*/
+
 /**
- * @description
+ * @description Represents a mapping of an object's properties to their respective property descriptors.
  * @export
- * @template Obj 
+ * @template Obj The type of the object whose property descriptors are being represented.
  */
 export type ObjectPropertyDescriptors<Obj> = {
   [P in keyof Obj]: TypedPropertyDescriptor<Obj[P]>;
-} & { [x: string]: PropertyDescriptor };
+} & { [key: string]: PropertyDescriptor };
