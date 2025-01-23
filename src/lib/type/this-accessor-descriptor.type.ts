fa6d3728f91252326a24813707dfb03516a43a35
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-23 18:36:18 +0000

    refactor(ThisAccessorDescriptor): change to `ThisAccessorPropertyDescriptor`.

--- src/lib/type/this-accessor-descriptor.type.ts
+++
@@ -1,9 +0,0 @@
-// Interface.
-import { AccessorDescriptor } from '../interface/accessor-descriptor.interface';
-/**
- * @description `AccessorDescriptor` interface as a type cause of ease of use `this` of an `Obj` type in the `get()` and `set()` methods.
- * @export
- * @template Value 
- * @template {object} Obj 
- */
-export type ThisAccessorDescriptor<Value, Obj extends object> = AccessorDescriptor<Value> & ThisType<Obj>;
