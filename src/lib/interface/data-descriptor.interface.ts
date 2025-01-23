f0428bce6a3f119a046072b5be016f87c70b6fcc
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-23 17:23:58 +0000

    docs(lib): update library documentation.

--- src/lib/interface/data-descriptor.interface.ts
+++ src/lib/interface/data-descriptor.interface.ts
@@ -1,12 +1,21 @@
 /**
- * @description
+ * @description Represents a data property descriptor, which describes the attributes of a property that holds a specific `value`.
  * @export
  * @interface DataDescriptor
- * @template Value 
+ * @template Value The type of the value held by the property.
  * @extends {Pick<PropertyDescriptor, 'configurable' | 'enumerable'>}
  */
 export interface DataDescriptor<Value>
-  extends Pick<PropertyDescriptor, 'configurable' | 'enumerable'> {
+  extends Pick<PropertyDescriptor, 'configurable' | 'enumerable'> {  
+  /**
+   * @description Indicates if the value of the property can be changed. Defaults to `false`.
+   * @type {?boolean}
+   */
   writable?: boolean;
+
+  /**
+   * @description The value of generic type variable `Value` associated with the property.
+   * @type {?Value}
+   */
   value?: Value;
 }
--- src/lib/type/any-descriptor.type.ts
+++ src/lib/type/any-descriptor.type.ts
@@ -3,7 +3,9 @@ import { DataDescriptor } from '../interface/data-descriptor.interface';
 // Type.
 import { ThisAccessorDescriptor } from './this-accessor-descriptor.type';
 /**
- * @description
+ * @description Represents a union of an accessor descriptor and a data descriptor. This type combines both getter/setter functions 
+ * from `ThisAccessorDescriptor` and the `value` and `writable` attributes from `DataDescriptor`. 
+ * This allows for properties that have both accessor behavior (via `get` and `set`) and data storage behavior (via `value` and `writable`).
  * @export
  * @template Value 
  * @template {object} [Obj=object] 
