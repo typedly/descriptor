d3139114b8f9b4b2087d7c27b0fb974ba258617c
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-23 18:35:32 +0000

    refactor(CommonDescriptor): change to `CommonPropertyDescriptor`.

--- src/lib/interface/common-property-descriptor.interface.ts
+++ src/lib/interface/common-property-descriptor.interface.ts
@@ -2,8 +2,8 @@
  * @description Common keys `configurable` and `enumerable` of a `boolean` type for accessor and data descriptor,
  * picked from the default `PropertyDescriptor`. "If a descriptor has neither of `value`, `writable`, `get` and `set` keys, it is treated as a data descriptor. "
  * @export
- * @interface CommonDescriptor
+ * @interface CommonPropertyDescriptor
  * @extends {Pick<PropertyDescriptor, 'configurable' | 'enumerable'>}
  */
-export interface CommonDescriptor
+export interface CommonPropertyDescriptor
   extends Pick<PropertyDescriptor, 'configurable' | 'enumerable'> {}
