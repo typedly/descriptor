39c94f1854bf57a9342419e33605a2dc9c6220b3
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-07-04 00:29:44 +0000

    chore(api): 4.0.0 update.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,4 +3,5 @@ export type { AnyPropertyDescriptor } from './type/any-property-descriptor.type'
 export type { CommonPropertyDescriptor } from './interface/common-property-descriptor.interface';
 export type { DataPropertyDescriptor } from './interface/data-property-descriptor.interface';
 export type { ObjectPropertyDescriptors } from './type/object-property-descriptors.type';
+export type { StrictPropertyDescriptor } from './type/strict-property-descriptor.type';
 export type { ThisAccessorPropertyDescriptor } from './type/this-accessor-property-descriptor.type';
