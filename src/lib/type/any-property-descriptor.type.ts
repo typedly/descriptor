22088370c80940814ae1ab2e7d87c35e74ffeb99
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-07-04 00:28:30 +0000

    docs(`AnyPropertyDescriptor`): update.

--- src/lib/type/any-property-descriptor.type.ts
+++ src/lib/type/any-property-descriptor.type.ts
@@ -3,7 +3,7 @@ import { DataPropertyDescriptor } from '../interface/data-property-descriptor.in
 // Type.
 import { ThisAccessorPropertyDescriptor } from './this-accessor-property-descriptor.type';
 /**
- * @description Represents a intersection of an accessor descriptor and a data descriptor. This type combines both getter/setter functions 
+ * @description Represents an intersection of an accessor descriptor and a data descriptor. This type combines both getter/setter functions 
  * from `ThisAccessorDescriptor` and the `value` and `writable` attributes from `DataDescriptor`. 
  * This allows for properties that have both accessor behavior (via `get` and `set`) and data storage behavior (via `value` and `writable`).
  * @export
