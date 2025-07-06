f530cd058c214cda3cdb592506e8e0b18b08a87e
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-07-06 19:15:11 +0000

    docs: update.

--- src/lib/interface/accessor-property-descriptor.interface.ts
+++ src/lib/interface/accessor-property-descriptor.interface.ts
@@ -2,13 +2,13 @@
 import { CommonPropertyDescriptor } from "./common-property-descriptor.interface";
 /**
  * @description Represents an accessor property descriptor with its unique optional `get()` and `set()` functions,
- * used to define or modify properties with getter and setter functions of the `Value` type.
- * For the accessor descriptor with also the object type, please use the type `ThisAccessorDescriptor<Value, Obj>`.
+ * used to define or modify properties with getter and setter functions of the `V` type.
+ * For the accessor descriptor with also the object type, please use the type `ThisAccessorDescriptor<V, O>`.
  * @export
  * @interface AccessorPropertyDescriptor
  * @template V The type of the value accessed by the property.
- * @template {boolean} [C=boolean] The type of the configurable.
- * @template {boolean} [E=boolean] The type of the enumerable.
+ * @template {boolean} [C=boolean]  The type of the configurable.
+ * @template {boolean} [E=boolean]  The type of the enumerable.
  * @extends {CommonPropertyDescriptor<C, E>}
  */
 export interface AccessorPropertyDescriptor<
@@ -29,3 +29,4 @@ export interface AccessorPropertyDescriptor<
    */
   set?: (value: V) => void;
 }
+
--- src/lib/interface/index.ts
+++ src/lib/interface/index.ts
@@ -1,3 +1,4 @@
 export type { AccessorPropertyDescriptor } from './accessor-property-descriptor.interface';
 export type { CommonPropertyDescriptor } from './common-property-descriptor.interface';
 export type { DataPropertyDescriptor } from './data-property-descriptor.interface';
+export type { DescriptorsShape } from './descriptors-shape.interface';
