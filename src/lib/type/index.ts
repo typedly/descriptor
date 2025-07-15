1920a2b90c386945bb2e83e0bc32180d421d0a78
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-07-15 07:14:08 +0000

    chore(api): update.

--- src/lib/type/index.ts
+++ src/lib/type/index.ts
@@ -1,5 +1,6 @@
 // Public.
 export type { AnyPropertyDescriptor } from './any-property-descriptor.type';
-export type { StrictPropertyDescriptor } from './strict-property-descriptor.type';
+export type { DescriptorsShape } from './descriptors-shape.interface';
 export type { ObjectPropertyDescriptors } from './object-property-descriptors.type';
+export type { StrictPropertyDescriptor } from './strict-property-descriptor.type';
 export type { ThisAccessorPropertyDescriptor } from './this-accessor-property-descriptor.type';
--- src/public-api.ts
+++ src/public-api.ts
@@ -11,6 +11,7 @@ export type {
 
   // Type.
   AnyPropertyDescriptor,
+  DescriptorsShape,
   ObjectPropertyDescriptors,
   StrictPropertyDescriptor,
   ThisAccessorPropertyDescriptor
