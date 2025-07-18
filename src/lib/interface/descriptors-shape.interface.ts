5ff3d7f568d56ec8d0d4f0181d0c48994d1c7449
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-07-18 07:26:35 +0000

    refactor(`DescriptorsShape`): add method `chain()` to get the descriptor chain of the specified `key`, update documentation.

--- src/lib/interface/descriptors-shape.interface.ts
+++ src/lib/interface/descriptors-shape.interface.ts
@@ -1,38 +1,41 @@
 // Interface.
-import { PropertyDescriptorChainShape } from '.';
+import { PropertyDescriptorChain } from './property-descriptor-chain-shape.interface';
 // Type.
 import { ThisAccessorPropertyDescriptor } from '../type/this-accessor-property-descriptor.type';
 /**
  * @description The shape of a property descriptors chain in the object.
  * @export
  * @interface DescriptorsShape
- * @template {object} [O=object] 
- * @template {keyof O} [K=keyof O] 
- * @template {K extends keyof O ? O[K] : any} [V=K extends keyof O ? O[K] : any] 
- * @template {boolean} [A=boolean] 
- * @template {boolean} [F=boolean] 
- * @template {boolean} [C=boolean] 
- * @template {boolean} [E=boolean] 
+ * @template {object} [O=object] The object type that the descriptors are associated with.
+ * @template {keyof O} [K=keyof O] The type of the property key.
+ * @template {K extends keyof O ? O[K] : any} [V=K extends keyof O ? O[K] : any] The type of the property value.
+ * @template {boolean} [A=boolean] The type of active property.
+ * @template {boolean} [ED=boolean] The type of enabled property.
+ * @template {boolean} [C=boolean] The type of configurable property.
+ * @template {boolean} [E=boolean] The type of enumerable property.
  * @template {ThisAccessorPropertyDescriptor<V, O, C, E>} [D=ThisAccessorPropertyDescriptor<V, O, C, E>] 
- * @template {PropertyDescriptorChainShape<O, K, V, A, F, C, E, D>} [DC=PropertyDescriptorChainShape<O, K, V, A, F, C, E, D>] 
+ * @template {PropertyDescriptorChain<O, K, V, A, ED, C, E, D>} [DC=PropertyDescriptorChain<O, K, V, A, ED, C, E, D>] 
  */
 export interface DescriptorsShape<
-  O extends object = object,
+  // Object.
+  O,
+  // Key.
   K extends keyof O = keyof O,
+  // Value.
   V extends K extends keyof O ? O[K] : any = K extends keyof O ? O[K] : any,
+  // Active.
   A extends boolean = boolean,
-  F extends boolean = boolean,
+  // Enabled.
+  ED extends boolean = boolean,
+  // Configurable.
   C extends boolean = boolean,
+  // Enumerable.
   E extends boolean = boolean,
+  // Descriptor.
   D extends ThisAccessorPropertyDescriptor<V, O, C, E> = ThisAccessorPropertyDescriptor<V, O, C, E>,
-  DC extends PropertyDescriptorChainShape<O, K, V, A, F, C, E, D> = PropertyDescriptorChainShape<O, K, V, A, F, C, E, D>,
+  // Descriptor Chain.
+  DC extends PropertyDescriptorChain<O, K, V, A, ED, C, E, D> = PropertyDescriptorChain<O, K, V, A, ED, C, E, D>,
 > {
-  /**
-   * @description
-   * @type {Readonly<DC>}
-   */
-  get chain(): Readonly<DC>;
-
   /**
    * @description Adds a new property descriptor to the chain.
    * @param {K} key The property key.
@@ -40,6 +43,12 @@ export interface DescriptorsShape<
    * @returns {this} 
    */
   add(key: K, value: D): this;
+
+  /**
+   * @description Gets the descriptor chain for the specified key.
+   * @type {Readonly<DC>}
+   */
+  chain(key: K): Readonly<DC>;
 
   /**
    * @description Clears the descriptors for the specified key.
@@ -53,11 +62,12 @@ export interface DescriptorsShape<
    * @returns {(D | undefined)} The current descriptor or undefined if not found.
    */
   current(key: K): D | undefined;
-
+  
   /**
    * @description Deletes the descriptor at the specified index for the given key.
    * @param {K} key The property key.
    * @param {number} index The index of the descriptor to delete.
+   * @returns {this} Returns the current instance for chaining.
    */
   delete(key: K, index: number): this;
 
@@ -78,8 +88,8 @@ export interface DescriptorsShape<
   has(key: K, index?: number): boolean;
 
   /**
-   * @description
-   * @returns {K[]} 
+   * @description Returns an array of keys for which descriptors exist.
+   * @returns {K[]} The returned value is an array of keys.
    */
   keys(): K[];
 
@@ -88,7 +98,7 @@ export interface DescriptorsShape<
    * @param {K} key The property key.
    * @param {number} index The index of the descriptor to set.
    * @param {D} value The property descriptor to set.
-   * @returns {this} 
+   * @returns {this} Returns the current instance for chaining.
    */
   set(key: K, index: number, value: D): this;
 
@@ -97,7 +107,7 @@ export interface DescriptorsShape<
    * @param {K} key The property key.
    * @param {number} index The index of the descriptor to update.
    * @param {D} value The property descriptor to update the existing one.
-   * @returns {this} 
+   * @returns {this} Returns the current instance for chaining.
    */
   update(key: K, index: number, value: D): this;
 }
