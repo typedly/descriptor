ae078224dbb46ee9217a0c6034f6ed4ab832cedf
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-07-27 12:53:20 +0000

    refactor(`WrappedPropertyDescriptor`): add generic type variable `D` for descriptor to handle previous and current descriptor in the `set` and `get`. Change the `ED` to `N`.

--- src/lib/interface/wrapped-property-descriptor.interface.ts
+++ src/lib/interface/wrapped-property-descriptor.interface.ts
@@ -9,9 +9,10 @@ import { ThisAccessorPropertyDescriptor } from '../type';
  * @template {keyof O} [K=keyof O] The key type constrained by the object `O`.
  * @template {K extends keyof O ? O[K] : any} [V=K extends keyof O ? O[K] : any] The type of the value accessed by the property, which is either the type of the property in the object or `any` if the key does not exist.
  * @template {boolean} [A=boolean] The type of active property, which can be a boolean or an object with `onGet` and `onSet` properties.
- * @template {boolean} [ED=boolean] The type of enabled property.
+ * @template {boolean} [N=boolean] The type of enabled property.
  * @template {boolean} [C=boolean] The type of configurable property.
  * @template {boolean} [E=boolean] The type of enumerable property.
+ * @template {WrappedPropertyDescriptor<O, K, V, A, N, C, E, D> | PropertyDescriptor} [D=PropertyDescriptor] 
  * @extends {Omit<ThisAccessorPropertyDescriptor<V, O, C, E>, 'set' | 'get'>}
  */
 export interface WrappedPropertyDescriptor<
@@ -24,23 +25,25 @@ export interface WrappedPropertyDescriptor<
   // Active.
   A extends boolean = boolean,
   // Enabled.
-  ED extends boolean = boolean,
+  N extends boolean = boolean,
   // Configurable.
   C extends boolean = boolean,
   // Enumerable.
   E extends boolean = boolean,
+  // Descriptor as previous or current in the `set` and `get`.
+  D extends WrappedPropertyDescriptor<O, K, V, A, N, C, E, D> | PropertyDescriptor = PropertyDescriptor
 > extends Omit<ThisAccessorPropertyDescriptor<V, O, C, E>, 'set' | 'get'> {
   /**
    * @description The `set` to wrap the original `set()` method for accessing the `descriptor`.
-   * @type {?(this: O, value: V, descriptor?: WrappedPropertyDescriptor<O, K, V, A, ED, C, E>) => void}
+   * @type {?(this: O, value: V, descriptor?: D) => void}
    */
-  set?: (this: O, value: V, descriptor?: WrappedPropertyDescriptor<O, K, V, A, ED, C, E>) => void;
+  set?: (this: O, value: V, descriptor?: D) => void;
   
   /**
    * @description The `get` to wrap the original `get()` method for accessing the `descriptor`.
-   * @type {?(this: O, descriptor?: WrappedPropertyDescriptor<O, K, V, A, ED, C, E>) => V}
+   * @type {?(this: O, descriptor?: D) => V}
    */
-  get?: (this: O, descriptor?: WrappedPropertyDescriptor<O, K, V, A, ED, C, E>) => V;
+  get?: (this: O, descriptor?: D) => V;
 
   /**
    * @description Whether the property descriptor `onGet` and `onSet` callbacks are active.
@@ -52,15 +55,21 @@ export interface WrappedPropertyDescriptor<
    * @description Whether the property is enabled.
    * If `true`, the property stores the value in the private key.
    * If `false`, the property does not store the value in the private key.
-   * @type {?ED}
+   * @type {?N}
    */
-  enabled?: ED;
+  enabled?: N;
+
+  /**
+   * @description The index of `number` type for chaining.
+   * @type {?number}
+   */
+  index?: number;
 
   /**
    * @description The previous descriptor of the property for unwrapping.
-   * @type {?(WrappedPropertyDescriptor<O, K, V, A, ED, C, E> | PropertyDescriptor)}
+   * @type {?|D}
    */
-  previousDescriptor?: WrappedPropertyDescriptor<O, K, V, A, ED, C, E> | PropertyDescriptor;
+  previousDescriptor?: D;
 
   /**
    * @description The key used to access the property in the object.
