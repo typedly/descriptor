76ed2939003d050efb04c5d24f16e0af49fa7621
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-09-12 13:48:16 +0000

    refactor(`DescriptorAttributes`): change the name for the specific `KeyedAccessorPropertyDescriptor`.

--- src/lib/interface/keyed-accessor-property-descriptor.interface.ts
+++ src/lib/interface/keyed-accessor-property-descriptor.interface.ts
@@ -1,20 +1,26 @@
 // Type.
 import { ThisAccessorPropertyDescriptor } from "../type/this-accessor-property-descriptor.type";
 /**
- * @description The attributes for the attributed property descriptor.
+ * @description The accessor property descriptor of the specified object `O` and key `K`.
  * @export
- * @interface DescriptorAttributes
+ * @interface KeyedAccessorPropertyDescriptor
  * @template [O=any] The type of the object.
  * @template {keyof O} [K=keyof O] The key of the object.
- * @template {K extends keyof O ? O[K] : any} [V=K extends keyof O ? O[K] : any] The value of the object.
+ * @template {K extends keyof O ? O[K] : any} [V=K extends keyof O ? O[K] : any] The value of the property.
  * @template {boolean} [C=boolean] Whether the property is configurable.
  * @template {boolean} [E=boolean] Whether the property is enumerable.
  * @extends {ThisAccessorPropertyDescriptor<V, O, C, E>}
  */
-export interface DescriptorAttributes<
+export interface KeyedAccessorPropertyDescriptor<
   O = any,
   K extends keyof O = keyof O,
   V extends K extends keyof O ? O[K] : any = K extends keyof O ? O[K] : any,
   C extends boolean = boolean,
   E extends boolean = boolean
-> extends ThisAccessorPropertyDescriptor<V, O, C, E> {};
+> extends ThisAccessorPropertyDescriptor<V, O, C, E> {
+  /**
+   * @description The concrete key used to access the property in the object.
+   * @type {?K}
+   */
+  key?: K;
+};
