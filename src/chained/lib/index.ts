cf8d59d6013ae91480d75c69f997c55b9917c49d
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-09-12 03:50:58 +0000

    chore(chained/index): update.

--- src/chained/lib/index.ts
+++ src/chained/lib/index.ts
@@ -1,3 +1,2 @@
 export type { ChainedPropertyDescriptor } from './chained-property-descriptor.type';
 export type { ChainedDescriptorAttributes } from './chained-descriptor-attributes.interface';
-export type { ChainedDescriptorChain } from './chained-descriptor-chain.interface';
