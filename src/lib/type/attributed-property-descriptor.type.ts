df237694ee7f31bf7652d42067d742984e549373
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-09-12 13:47:41 +0000

    refactor(`AttributedDescriptor`): change the name to `AttributedPropertyDescriptor`.

--- src/lib/type/attributed-property-descriptor.type.ts
+++ src/lib/type/attributed-property-descriptor.type.ts
@@ -1,16 +1,15 @@
 // Interface.
-import { DescriptorAttributes } from "../interface/descriptor-attributes.interface";
+import { KeyedAccessorPropertyDescriptor } from "../interface/keyed-accessor-property-descriptor.interface";
 /**
  * @description The attributed property descriptor for attributes customization.
- * @export
  * @template [O=any] The type of the object that `this` refers to in the `get()` and `set()` methods.
  * @template {keyof O} [K=keyof O] The key type constrained by the object `O`.
  * @template {K extends keyof O ? O[K] : any} [V=K extends keyof O ? O[K] : any] The value type of the property.
- * @template {DescriptorAttributes<O, K, V, boolean, boolean>} [A=DescriptorAttributes<O, K, V, boolean, boolean>] The attributes for the descriptor.
+ * @template {KeyedAccessorPropertyDescriptor<O, K, V, boolean, boolean>} [A=KeyedAccessorPropertyDescriptor<O, K, V, boolean, boolean>] The attributes for the descriptor.
  */
-export type AttributedDescriptor<
+export type AttributedPropertyDescriptor<
   O = any,
   K extends keyof O = keyof O,
   V extends K extends keyof O ? O[K] : any = K extends keyof O ? O[K] : any,
-  A extends DescriptorAttributes<O, K, V, boolean, boolean> = DescriptorAttributes<O, K, V, boolean, boolean>,
+  A extends KeyedAccessorPropertyDescriptor<O, K, V, boolean, boolean> = KeyedAccessorPropertyDescriptor<O, K, V, boolean, boolean>,
 > = A;
