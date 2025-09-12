c5435a7ee6bd9b6456dd07ac1700865ab0dc0155
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-09-12 03:15:56 +0000

    chore(wrapped/index): update.

--- src/wrapped/lib/index.ts
+++ src/wrapped/lib/index.ts
@@ -1,3 +1,3 @@
-export type { ChainedPropertyDescriptor as ChainedDescriptor } from './chained-property-descriptor.type';
+export type { ChainedPropertyDescriptor } from './chained-property-descriptor.type';
 export type { ChainedDescriptorAttributes } from './chained-descriptor-attributes.interface';
 export type { ChainedDescriptorChain } from './chained-descriptor-chain.interface';
